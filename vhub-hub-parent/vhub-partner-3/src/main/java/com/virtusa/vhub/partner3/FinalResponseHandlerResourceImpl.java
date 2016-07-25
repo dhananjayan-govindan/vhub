cd849b8a1c2ae23a2b378f083e6a6b617870b6e3
chatdhana <chatdhana@gmail.com>
2016-07-25 10:43:08 +0530

    Adding partner-2 and partner-3 modules

---
+++ vhub-hub-parent/vhub-partner-3/src/main/java/com/virtusa/vhub/partner3/FinalResponseHandlerResourceImpl.java
@@ -0,0 +1,17 @@
+package com.virtusa.vhub.partner3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FinalResponseHandlerResourceImpl implements
+		FinalResponseHandlerResource {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(FinalResponseHandlerResourceImpl.class);
+
+	@Override
+	public void processFinalResponse(String commandResponse) {
+		log.info("Final response received is: {}", commandResponse);
+	}
+
+}
